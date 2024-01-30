0c06be9a24e5451955248c6065b118fd68344f08
2018331113 <muhitmahmud404@gmail.com>
2024-01-31 03:50:22 +0600

    created class for query

---
+++ lib/models/query.dart
@@ -0,0 +1,28 @@
+class Query {
+  final String q;
+  String? sort;
+  String? order;
+  int page;
+  int perPage;
+
+  Query({
+    required this.q,
+    this.sort,
+    this.order,
+    this.page =1,
+    this.perPage = 30,
+  });
+
+
+
+  Map<String, dynamic> toMap() {
+    return {
+      'q': q,
+      'sort': sort,
+      'order': order,
+      'page': page,
+      'per_page': perPage,
+    };
+    
+  }
+}
