7cfb4b0ac8e619250c735cb010f36979317a54bd
2018331113 <muhitmahmud404@gmail.com>
2024-01-31 01:14:37 +0600

    added routes for navigation

---
+++ lib/config/routes.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+import '../screens/screens.dart';
+
+class RouteArguments<T> {
+  final T? item;
+  final VoidCallback? callback;
+  RouteArguments({this.item, this.callback});
+}
+
+class Routes {
+  static const String home = "/home";
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home:
+        return MaterialPageRoute(
+          builder: (context) {
+            return const HomeScreen();
+          },
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (context) {
+            return const DefaultScreen();
+          },
+        );
+    }
+  }
+
+  static final Routes _instance = Routes._internal();
+
+  factory Routes() {
+    return _instance;
+  }
+
+  Routes._internal();
+}
+
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export './default.dart';
+export './home.dart';
