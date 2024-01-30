fc09da1c69c81a8ce637e0fd71c2dc42481e7fe8
2018331113 <muhitmahmud404@gmail.com>
2024-01-31 02:50:38 +0600

    called api

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../api/api.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -8,6 +10,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    Api().getRepositories({
+      "q": "topic:Flutter",
+      "sort": "stars",
+      "order": "desc",
+      "page": 1,
+      "per_page": 10,
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
