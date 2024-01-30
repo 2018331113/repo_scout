5a7dd015da59040532707e9d7811e4945a763cdc
2018331113 <muhitmahmud404@gmail.com>
2024-01-31 03:51:48 +0600

    tried fetching and updating ui

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:repo_scout/repository/remote_repository.dart';
 
 import '../api/api.dart';
+import '../models/query.dart';
+import '../models/repo.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,24 +13,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
+  List<Repo>? repos;
   @override
   void initState() {
-    Api().getRepositories({
-      "q": "topic:Flutter",
-      "sort": "stars",
-      "order": "desc",
-      "page": 1,
-      "per_page": 10,
-    });
+    getData();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home"),
-      )
+    return Scaffold(
+        body: Center(
+      child: (repos != null)
+          ? ListView.builder(
+            itemCount: repos!.length,
+            itemBuilder: (context, index) {
+
+              return Text(repos![index].name);
+            })
+          : CircularProgressIndicator(),
+    ));
+  }
+
+  Future getData() async {
+    repos = await RemoteRepository().getRepositories(
+      Query(
+        q: "topic:flutter",
+        sort: "stars",
+        order: "desc",
+        page: 1,
+        perPage: 10,
+      ),
     );
+    setState(() {
+      
+    });
   }
 }
