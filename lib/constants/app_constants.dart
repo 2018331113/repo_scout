fdbbe9f3b67f495c5bc7fa1a434942a032aba9cc
2018331113 <muhitmahmud404@gmail.com>
2024-01-31 15:36:38 +0600

    created a file for holding all ui constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+const SizedBox vGap5 = SizedBox(
+  height: 5,
+);
+
+const SizedBox vGap10 = SizedBox(
+  height: 10,
+);
+
+const SizedBox vGap20 = SizedBox(
+  height: 20,
+);
+
+const SizedBox vGap30 = SizedBox(
+  height: 30,
+);
+
+const SizedBox vGap50 = SizedBox(
+  height: 50,
+);
+
+
+const SizedBox hGap5 = SizedBox(
+  width: 5,
+);
+const SizedBox hGap10 = SizedBox(
+  width: 10,
+);
+
+const SizedBox hGap20 = SizedBox(
+  width: 20,
+);
+
+const SizedBox hGap30 = SizedBox(
+  width: 30,
+);
+
+const SizedBox hGap50 = SizedBox(
+  width: 50,
+);
+
+const TextStyle titleStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  fontSize: 18,
+);
+
